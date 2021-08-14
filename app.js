9ae097aec09c203662dc943125ab2f09d28bdc2a
Kshitij Kumar <1234567890kshitij@gmail.com>
2021-08-14 11:45:53 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,122 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+
+const LocalStrategy = require("passport-local").Strategy;
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userAuthDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+mongoose.set("useCreateIndex", true);
+
+// ******************* MODEL FOR USERS *******************
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  username: { type: String, unique: true, required: true },
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// ******************* ROUTES *******************
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.get("/CompanyXYZ", function (req, res) {
+  User.find({ secret: { $ne: null } }, function (err, foundUsers) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("CompanyXYZ", { userWithCompanyXYZ: foundUsers });
+      }
+    }
+  });
+});
+
+
+app.get("/logout", function (req, res) {
+  req.logOut();
+  res.redirect("/");
+});
+
+app.post("/register", function (req, res) {
+  var user = new User({ name: req.body.name, username: req.body.username });
+
+  User.register(user, req.body.password, function (err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/CompanyXYZ");
+      });
+    }
+  });
+});
+
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.logIn(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/CompanyXYZ");
+      });
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
